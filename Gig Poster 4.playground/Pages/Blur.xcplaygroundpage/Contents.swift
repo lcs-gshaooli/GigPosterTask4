1bdbfdd3d8d450691d2c628d0e8ba179bc6b0ba4
Russell Gordon <github@russellgordon.ca>
2020-01-21 20:22:40 -0500

    Initial Commit

---
+++ Gig Poster 4.playground/Pages/Blur.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: # Gig Poster 4
+//: The following two statements are required to make the playground run. Please do not remove.
+import Cocoa
+import PlaygroundSupport
+
+/*:
+ Each image is exactly 400 pixels wide by 600 pixels tall.
+ 
+ Your goal is to precisely reproduce this image:
+ 
+ ![blur-no-grid](blur-no-grid.png "Blur")
+ ![blur-with-grid](blur-with-grid.png "Blur")
+ 
+ By precisely reproducing this image, you will demonstrate your understanding of:
+ 
+ * sequence (order of statements in a program)
+ * conditionals (making decisions with ``if`` statements)
+ * iteration (use of loops to repeat statements)
+ 
+ You may use the color constants created below to obtain the correct colors.
+ 
+ Remember that you can refer to the [Canvas class documentation](https://www.russellgordon.ca/canvasgraphics/documentation/classes/canvas), as needed.
+ */
+
+// Create a new canvas
+let canvas = Canvas(width: 400, height: 600)
+
+// COLORS
+let lightBlue = Color(hue: 193, saturation: 81, brightness: 84, alpha: 100)
+
+// Begin your solution here...
+
+
+/*:
+ ## Use Source Control
+ 
+ Remember to commit and push your work before the end of evening study on Tuesday, January 21, 2020, please.
+
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas
+
